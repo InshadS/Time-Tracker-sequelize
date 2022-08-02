e7e41dc81c0e698834514bf5d8e62d1568f57cae
InshadS <inshad@hamon.in>
2022-08-02 16:04:01 +0530

    Added API for github authentication

--- routes/passportAuth.js
+++ routes/passportAuth.js
@@ -23,11 +23,23 @@ router.get('/logout', (req, res) => {
   res.redirect(CLIENT_LOGIN_URL);
 });
 
+// Google
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 
 router.get(
   '/google/callback',
   passport.authenticate('google', {
+    successRedirect: CLIENT_URL,
+    failureRedirect: '/login/failed',
+  })
+);
+
+// GitHub
+router.get('/github', passport.authenticate('github', { scope: ['profile'] }));
+
+router.get(
+  '/github/callback',
+  passport.authenticate('github', {
     successRedirect: CLIENT_URL,
     failureRedirect: '/login/failed',
   })
